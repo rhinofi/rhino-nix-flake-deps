6cfd55816ed8fb40db5dc1e339db617a9466cadf
Adrian Gierakowski <agierakowski@gmail.com>
2023-07-19 08:29:08 +0100

    services-flake: use rhinofi fork

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    services-flake.url = "github:juspay/services-flake";
+    services-flake.url = "github:rhinofi/services-flake/rhinofi";
     flake-compat = {
       url = "github:rhinofi/flake-compat/lock-file-overrides";
       flake = false;
