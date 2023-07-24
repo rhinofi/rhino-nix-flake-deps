1efdf7d35367c615722399abc834a71a9a80e5ee
Adrian Gierakowski <agierakowski@gmail.com>
2023-07-24 11:29:51 +0100

    nixpkgs => nixpkgs-lib

--- flake.nix
+++ flake.nix
@@ -4,8 +4,11 @@
     # NOTE: the specific url here doesn't matter as it will be overriden
     # using lockFileProcessorFunc passed to default.nix. This way the consumer
     # of this flake, can decide which nixpkgs to use.
-    nixpkgs.url = "github:nixos/nixpkgs/8e3fad82be64c06fbfb9fd43993aec9ef4623936";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs-lib.url = "github:nixos/nixpkgs/8e3fad82be64c06fbfb9fd43993aec9ef4623936?dir=lib";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    };
     systems.url = "github:nix-systems/default";
     process-compose-flake.url = "github:rhinofi/process-compose-flake/rhinofi";
     services-flake.url = "github:rhinofi/services-flake/rhinofi";
